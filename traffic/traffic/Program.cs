e7441834f0a3fb64a58836bdaacb5445ce2b8899
tibbe2003 <76557818+tibbe2003@users.noreply.github.com>
2021-12-07 17:23:09 +0100

    traffic_light

---
+++ traffic/traffic/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace traffic
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            TrafficLight trafficLight = new TrafficLight();
+            // color has to be "Red".
+            Console.WriteLine(trafficLight.GetCurrentcolor());
+            trafficLight.NextState();
+            // color has to be "green".
+            Console.WriteLine(trafficLight.GetCurrentcolor());
+            trafficLight.NextState();
+            // color has to be "oranje".
+            Console.WriteLine(trafficLight.GetCurrentcolor());
+            trafficLight.NextState();
+            // and 'red' again!
+            Console.WriteLine(trafficLight.GetCurrentcolor());
+        }
+    }
+}
---
+++ traffic/traffic/TrafficLight.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace traffic
+{
+    internal class TrafficLight
+    {
+        public string color { get; private set; }
+
+        public TrafficLight()
+        {
+            this.color = "red";
+        }
+
+        public void NextState()
+        {
+            switch(color)
+            {
+                case "red": color = "green";
+                    break;
+                case "green": color = "orange";
+                    break;
+                case "orange": color = "red";
+                    break;
+            }
+        }
+
+        public string GetCurrentcolor()
+        {
+            return this.color;
+        }
+    }
+}
