f831d31fa275e073d5d77e2daff3d35435f105db
tibbe2003 <76557818+tibbe2003@users.noreply.github.com>
2021-12-07 11:47:52 +0100

    hero-tegen-monster

---
+++ hero-monsters/hero tegen monsters/hero tegen monsters/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using hero_tegen_monsters;
+
+namespace hero_tegen_monsters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            hero Hero = new hero();
+            monster Monster = new monster(100);
+            monster Monster2 = new monster(125);
+
+            Hero.Attack(Monster, 50);
+
+            Console.WriteLine(Monster.health);
+        }
+    }
+}
---
+++ hero-monsters/hero tegen monsters/hero tegen monsters/hero.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hero_tegen_monsters
+{
+    internal class hero
+    {
+        public int numberOfDefeatedMonsters;
+
+        public hero()
+        {
+
+        }
+
+        public void Attack(monster Monster, int damage)
+        {
+            Monster.loseHealth(damage);
+        }
+    }
+}
